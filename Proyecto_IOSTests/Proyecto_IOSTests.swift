68894b1e9815bc5188c2ca73ec34e9a0293534a2
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-09-06 07:32:42 -0700

    Initial Commit

---
+++ Proyecto_IOSTests/Proyecto_IOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Proyecto_IOSTests.swift
+//  Proyecto_IOSTests
+//
+//  Created by Alumno on 9/6/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import XCTest
+@testable import Proyecto_IOS
+
+class Proyecto_IOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
