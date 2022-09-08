f01b0313229ede4631b970440d941e6c41533d8e
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-09-08 08:57:16 -0700

    shiba

--- Proyecto_IOS/ViewController.swift
+++ Proyecto_IOS/ViewController.swift
@@ -13,27 +13,38 @@ class ViewController: UIViewController {
     
     var reproductor: AVAudioPlayer?
     
-
+    @IBOutlet weak var imgShiba: UIImageView!
+    
     @IBOutlet weak var lblLabelDogos: UILabel!
+    
+    var secuenciaPerro : [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         do{
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try
-                AVAudioSession.sharedInstance().setActive(true)
+            try AVAudioSession.sharedInstance().setActive(true)
         }
         catch let error {
             print(error.localizedDescription)
         }
         
+        for i in 1...4 {
+            let imagen = UIImage(named: "ShibaIDLE\(i)")
+            secuenciaPerro.append(imagen!)
+        }
+        
+        imgShiba.animationImages = secuenciaPerro
+        imgShiba.animationDuration = 1
+        imgShiba.startAnimating()
     }
+    
 
 
     @IBAction func DoTapDOGO(_ sender: Any) {
         
-        lblLabelDogos.text = "Prueba"
+        lblLabelDogos.text = "Shiba"
         do {
             let url = Bundle.main.url(forResource: "uff", withExtension: "mp3")
             
