0ecc6a4459501c76a20281a600bddb2fe64ce9c2
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-09-08 08:37:28 -0700

    responsive_landscape

--- Proyecto_IOS/ViewController.swift
+++ Proyecto_IOS/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var reproductor: AVAudioPlayer?
+    
 
+    @IBOutlet weak var lblLabelDogos: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        do{
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try
+                AVAudioSession.sharedInstance().setActive(true)
+        }
+        catch let error {
+            print(error.localizedDescription)
+        }
+        
     }
 
 
+    @IBAction func DoTapDOGO(_ sender: Any) {
+        
+        lblLabelDogos.text = "Prueba"
+        do {
+            let url = Bundle.main.url(forResource: "uff", withExtension: "mp3")
+            
+            reproductor = try AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            reproductor?.play()
+        }
+        catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
 }
 
