9aa8c12285722372713c25aa181b96e153972c1f
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-09-08 09:29:22 -0700

    Husky

--- Proyecto_IOS/ViewController.swift
+++ Proyecto_IOS/ViewController.swift
@@ -14,11 +14,16 @@ class ViewController: UIViewController {
     var reproductor: AVAudioPlayer?
     
     @IBOutlet weak var imgShiba: UIImageView!
+    @IBOutlet weak var imgHusky: UIImageView!
+    @IBOutlet weak var imgPrincipal: UIImageView!
     
     @IBOutlet weak var lblLabelDogos: UILabel!
     
+    var secuenciaShibaLadrando : [UIImage] = []
+    var secuenciaHuskyLadrando : [UIImage] = []
     var secuenciaPerro : [UIImage] = []
-    
+    var secuenciaHusky : [UIImage] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,23 +35,62 @@ class ViewController: UIViewController {
             print(error.localizedDescription)
         }
         
-        for i in 1...4 {
+        for i in 1...5 {
             let imagen = UIImage(named: "ShibaIDLE\(i)")
             secuenciaPerro.append(imagen!)
         }
         
+        for i in 1...5 {
+            let imagen2 = UIImage(named: "huskyIDLE\(i)")
+            secuenciaHusky.append(imagen2!)
+        }
+        
+        for i in 1...6 {
+            let imagen3 = UIImage(named: "Shiba\(i)")
+            secuenciaShibaLadrando.append(imagen3!)
+        }
+        
+        for i in 1...6 {
+            let imagen4 = UIImage(named: "Husky\(i)")
+            secuenciaHuskyLadrando.append(imagen4!)
+        }
+        
         imgShiba.animationImages = secuenciaPerro
-        imgShiba.animationDuration = 1
+        imgShiba.animationDuration = 1.5
         imgShiba.startAnimating()
+        
+        imgHusky.animationImages = secuenciaHusky
+        imgHusky.animationDuration = 1.5
+        imgHusky.startAnimating()
+        
+        imgPrincipal.animationDuration = 1.5
+    }
+    
+    @IBAction func DoTapHusky(_ sender: Any) {
+        lblLabelDogos.text = "Husky"
+        imgPrincipal.stopAnimating()
+        imgPrincipal.animationImages = secuenciaHuskyLadrando
+        imgPrincipal.startAnimating()
+         do {
+             let url = Bundle.main.url(forResource: "", withExtension: "mp3")
+             
+             reproductor = try AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.mp3.rawValue)
+             
+             reproductor?.play()
+         }
+         catch let error {
+             print(error.localizedDescription)
+         }
     }
     
 
-
     @IBAction func DoTapDOGO(_ sender: Any) {
-        
         lblLabelDogos.text = "Shiba"
+        imgPrincipal.stopAnimating()
+        imgPrincipal.animationImages = secuenciaShibaLadrando
+        imgPrincipal.startAnimating()
         do {
-            let url = Bundle.main.url(forResource: "uff", withExtension: "mp3")
+            let url = Bundle.main.url(forResource: "", withExtension: "mp3")
             
             reproductor = try AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.mp3.rawValue)
             
